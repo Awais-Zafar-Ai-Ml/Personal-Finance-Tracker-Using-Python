1ff5378a574e1bc1d873b0a8bde4d133b0e39020
Awais Zafar <awais03138825@gmail.com>
2026-08-31 23:33:43 +0500

    Main project structure

---
+++ expense_tracker.py
@@ -0,0 +1,32 @@
+def main():
+    print("Running Expense Tracker")
+    #Get user input for expense.
+    get_user_expense()
+    #Write their expense to a file.
+    save_expense_to_file()
+    #Read file and summarize expenses.
+    summarize_expense()
+    
+
+
+def get_user_expense():
+    print("Getting user expense")
+    
+
+
+
+def save_expense_to_file():
+    print("saving user expense")
+    
+
+
+
+def summarize_expense():
+    print("Summarizing user expense")
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
