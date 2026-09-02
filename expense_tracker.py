e5f89a32047a15229ebe379ffcf83a43c13e16da
Awais Zafar <awais03138825@gmail.com>
2026-09-03 01:26:33 +0500

    Fix bugs that will cause the program to crash

--- expense_tracker.py
+++ expense_tracker.py
@@ -21,7 +21,12 @@ def main():
 def get_user_expense():
     print("Getting user expense")
     expense_name=input("Enter expense name:")
-    expense_amount=float(input("Enter expense amount:"))
+    while True:
+        try:
+          expense_amount = float(input("Enter expense amount:"))
+          break
+        except ValueError:
+           print("Please enter a valid number.")
     
     expense_categories=[
         "Food",
@@ -38,11 +43,20 @@ def get_user_expense():
             print(f"{i + 1}. {category_name}")
 
         value_range=f"1-{len(expense_categories)}"
-        selected_index=int(input(f"Enter a category number {value_range}:")) - 1
+
+        try:
+         selected_index = int(input(f"Enter a category number {value_range}:")) - 1
+        except ValueError:
+          print("Please enter a number.")
+          continue
+
         if selected_index in range(len(expense_categories)):
+         
          selected_category=expense_categories[selected_index]
+
          new_expense = Expense(name=expense_name, category=selected_category, amount=expense_amount)
          return new_expense
+        
         else:
             print("Invalid Category. Please Try Again!")
 
@@ -65,13 +79,13 @@ def summarize_expense(expense_file_path ,budget):
     with open (expense_file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
-            expense_name, expense_amount , expense_catergory =line.strip().split(",")
+            expense_name, expense_amount , expense_category =line.strip().split(",")
            
             line_expense=Expense(
 
                 name = expense_name, 
                 amount=float(expense_amount), 
-                category=expense_catergory
+                category=expense_category
 
             )
             
@@ -102,9 +116,11 @@ def summarize_expense(expense_file_path ,budget):
     days_in_month= calendar.monthrange(now.year , now.month)[1]
     remaining_days = days_in_month - now.day
 
-    daily_budget = remaining_budget/remaining_days
-    print(f"Budget per day: {daily_budget:.2f}")
-
+    if remaining_days > 0:
+        daily_budget = remaining_budget / remaining_days
+        print(f"Budget per day: {daily_budget:.2f}")
+    else:
+        print("No days remaining in this month.")
 
 
 
